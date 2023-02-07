69e20724823e491dc353133e314bb34a7e60f250
myeonghyejin <myeonghyejin@icloud.com>
2023-02-07 22:29:37 +0900

    23_0207

--- src/main/java/com/mhj/s1/product/ProductDTO.java
+++ src/main/java/com/mhj/s1/product/ProductDTO.java
@@ -2,36 +2,35 @@ package com.mhj.s1.product;
 
 public class ProductDTO {
 	
-	private Long productNum;
-	private String productName;
-	private String productDetail;
-	private Double productRating;
-	
-	//getter, setter
-	
-	public Long getProductNum() {
-		return productNum;
+	   private Long productNum;
+	   private String productName;
+	   private String productDetail;
+	   private Double productJumsu;
+	   
+	   //getter, setter
+	   public Long getProductNum() {
+	      return productNum;
+	   }
+	   public void setProductNum(Long productNum) {
+	      this.productNum = productNum;
+	   }
+	   public String getProductName() {
+	      return productName;
+	   }
+	   public void setProductName(String productName) {
+	      this.productName = productName;
+	   }
+	   public String getProductDetail() {
+	      return productDetail;
+	   }
+	   public void setProductDetail(String productDetail) {
+	      this.productDetail = productDetail;
+	   }
+	   public Double getProductJumsu() {
+	      return productJumsu;
+	   }
+	   public void setProductJumsu(Double productJumsu) {
+	      this.productJumsu = productJumsu;
+	   }
+	   
 	}
-	public void setProductNum(Long productNum) {
-		this.productNum = productNum;
-	}
-	public String getProductName() {
-		return productName;
-	}
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-	public String getProductDetail() {
-		return productDetail;
-	}
-	public void setProductDetail(String productDetail) {
-		this.productDetail = productDetail;
-	}
-	public Double getProductRating() {
-		return productRating;
-	}
-	public void setProductRating(Double productRating) {
-		this.productRating = productRating;
-	}
-
-}
